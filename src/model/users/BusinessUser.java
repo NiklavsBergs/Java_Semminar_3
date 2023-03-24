d6d0728675d31cf8063dcc132011fab4e8907974
Niklāvs Bergs <s21bergnikl@vea.lv>
2023-03-24 16:12:24 +0200

    updated business user

--- src/model/users/BusinessUser.java
+++ src/model/users/BusinessUser.java
@@ -3,25 +3,54 @@ package model.users;
 import java.util.ArrayList;
 
 import model.Page;
+import model.Post;
+import model.PostType;
 
 public class BusinessUser extends User{
-	private ArrayList<Page> listOfPages;
+	private ArrayList<Page> allPages = new ArrayList<Page>();
 	
 	BusinessUser(){
 		super();
-		ArrayList<Page> listOfPages = new ArrayList<Page>();
 	}
 	
-	BusinessUser(String nameAndSurname, String username, String password){
-		super(nameAndSurname, username, password);
-		ArrayList<Page> listOfPages = new ArrayList<Page>();
+	BusinessUser(String username, String name, String surname,  String password){
+		super(username, name, surname, password);
 	}
 	
-	public void createPage(String title, String description) {
-		listOfPages.add(new Page(title, description));
+	public void addPage(Page page) {
+		if(page!=null && !allPages.contains(page)) {
+			allPages.add(page);
+		}
+	}
+	
+	public void removePage(Page page) {
+		if(page!=null && allPages.contains(page)) {
+			allPages.remove(page);
+		}
 	}
 	
 	public String toString() {
 		return "" + super.toString();
 	}
+
+	@Override
+	public Post createPost(Post post, PostType type) {
+		if(post != null) {
+			return post;
+		}
+		else {
+			return new Post ();
+		}
+	}
+
+	public ArrayList<Page> getAllPages() {
+		return allPages;
+	}
+	
+	public void createPostInPage(Page page, Post post) {
+		if(page != null && post != null && !page.getPostsInPage().contains(post)) {
+			page.getPostsInPage().add(post);
+		}
+	}
+	
 }
